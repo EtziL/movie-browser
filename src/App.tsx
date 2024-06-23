d908765c28d878c47a8c2bbb09271feeb6f9de3e
Lukáš Etzler <72432847+EtziL@users.noreply.github.com>
2024-06-23 21:55:07 +0200

    Added Netlify redirects and root path prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import { Toast, MovieList, PageNotFound, MovieDetail, TopBar } from './components/index'
+import { HiArrowUp } from 'react-icons/hi2'
 
 function App() {
-    // TODO:  come-up with better root path | Unit / Integration tests
-
     return (
         <div className="min-h-screen w-full bg-gradient-to-t from-neutral-900 via-offBlack to-neutral-800 py-6 font-inter sm:pl-[calc(100vw-100%)]">
             <div className="container mx-auto flex h-full flex-col px-6">
@@ -11,7 +10,14 @@ function App() {
                 <TopBar />
                 <div className="relative mt-12 flex-1">
                     <Routes>
-                        <Route path="/" element={<></>} />
+                        <Route
+                            path="/"
+                            element={
+                                <div>
+                                    <HiArrowUp className="mx-auto animate-bounce text-4xl text-white" />
+                                </div>
+                            }
+                        />
                         <Route path="/search/:searchedName/page/:currentPage" element={<MovieList />} />
                         <Route path="/detail/:imdbID" element={<MovieDetail />} />
                         <Route path="/favourites/page/:currentPage" element={<MovieList />} />
